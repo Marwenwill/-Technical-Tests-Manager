a606a96135c627fa2e2ac52d121903c3f7866fe8
Marwenwill <marouene.chouri@ensi-uma.tn>
2018-11-20 23:07:05 +0200

    Fourth commit

--- src/main/java/com/ensi/ilsi/TechnicalTesting/Web/ManagerResource.java
+++ src/main/java/com/ensi/ilsi/TechnicalTesting/Web/ManagerResource.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ensi.ilsi.TechnicalTesting.Domain.Web;
+package com.ensi.ilsi.TechnicalTesting.Web;
 
 /**
  *
