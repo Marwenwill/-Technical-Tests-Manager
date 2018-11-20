9b140098586fd8cc1da0ed80261c74cb041cb10a
Marwenwill <marouene.chouri@ensi-uma.tn>
2018-11-20 23:05:30 +0200

    added Web

---
+++ src/main/java/com/ensi/ilsi/TechnicalTesting/Domain/Web/ManagerResource.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ensi.ilsi.TechnicalTesting.Domain.Web;
+
+/**
+ *
+ * @author Toshiba-PC
+ */
+public class ManagerResource {
+    
+}
