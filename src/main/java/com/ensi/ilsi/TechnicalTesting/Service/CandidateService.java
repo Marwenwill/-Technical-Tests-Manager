1a353e21bf2ef235dff77703d18103e3194bae10
ahmadyghada <45200694+ahmadyghada@users.noreply.github.com>
2018-11-20 22:26:16 +0000

    Update CandidateService.java

--- src/main/java/com/ensi/ilsi/TechnicalTesting/Service/CandidateService.java
+++ src/main/java/com/ensi/ilsi/TechnicalTesting/Service/CandidateService.java
@@ -15,6 +15,11 @@ public class CandidateService {
         this.candidateRepository = candidateRepository;
     }
     
+    public Test ViewAffectedTest()
+    {
+        return Candidate.getTestList()
+    }
+    
     
    
 }
